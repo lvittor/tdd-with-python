52431f6c77025cb21382c7a32f683008fcd92ab8
lvittor <lvittor@itba.edu.ar>
2021-10-03 18:14:14 -0300

    add tests

---
+++ tests/test_ellipse.py
@@ -0,0 +1,29 @@
+import pytest
+
+from src.Point import Point
+from src.Ellipse import Ellipse
+
+ellipses = [
+    {"center": Point(0, 0), "sMayorAxis": 4, "sMinorAxis": 2},
+    {"center": Point(3, -1), "sMayorAxis": 5, "sMinorAxis": 1}
+]
+
+@pytest.fixture(scope="module", params=ellipses)
+def setup(request):
+    ellipse = Ellipse(request.param["center"], request.param["sMayorAxis"], request.param["sMinorAxis"])
+    yield ellipse
+
+def test_area(setup):
+    assert pytest.approx(setup.area(), 0.1) in [25.1, 15.7]
+
+def test_perimeter(setup):
+    assert pytest.approx(setup.perimeter(), 0.1) in [19.8, 22.6]
+
+def test_str(setup):
+    assert f"{setup}" in [
+                            "Ellipse [Center: Point [x:0.00, y:0.00], sMayorAxis: 4.00, sMinorAxis: 2.00]",
+                            "Ellipse [Center: Point [x:3.00, y:-1.00], sMayorAxis: 5.00, sMinorAxis: 1.00]"
+                         ]
+
+def test_is(setup):
+    assert isinstance(setup, Ellipse)
---
+++ tests/test_rectanagle.py
@@ -0,0 +1,29 @@
+import pytest
+
+from src.Point import Point
+from src.Rectangle import Rectangle
+
+rectangles = [
+    {"p1": Point(0, 0), "p2": Point(5, 5)},
+    {"p1": Point(2, 3), "p2": Point(-5, 1)}
+]
+
+@pytest.fixture(scope="module", params=rectangles)
+def setup(request):
+    rectangle = Rectangle(request.param["p1"], request.param["p2"])
+    yield rectangle
+
+def test_area(setup):
+    assert setup.area() in [25, 14]
+
+def test_perimeter(setup):
+    assert setup.perimeter() in [20, 18]
+
+def test_str(setup):
+    assert f"{setup}" in [
+                            "Rectangle [Point [x:0.00, y:0.00], Point [x:5.00, y:5.00]]",
+                            "Rectangle [Point [x:2.00, y:3.00], Point [x:-5.00, y:1.00]]"
+                         ]
+
+def test_is(setup):
+    assert isinstance(setup, Rectangle)
